4efc09687b8c8933e78a61f3794f375969d52a3a
Roshan Joseph <roshanjosephse01@gmail.com>
2022-12-13 22:30:03 +0530

    first commit

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+function Modal(props) {
+  function confirmHandler(){
+    props.onConfirm();
+  }
+  function cancelHandler(){
+    props.onCancel();
+  }
+  return <div className='modal'>
+    <p>Are you sure</p>
+    <button className='btn btn--alt' onClick={confirmHandler}>Confirm</button>
+    <button className='btn' onClick={cancelHandler} >Cancel</button>
+  </div>;
+}
+
+export default Modal;
